77e856c412fbfd1b417a3a6ecf9a218de9b9af60
Vladimir Rodkin <mail@vovanr.com>
2016-01-29 16:26:06 +0300

    Init

---
+++ index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const IndicatorLegend = React.createClass({
+	render() {
+		return (
+			<div {...this.props}>
+				Hello World!
+			</div>
+		);
+	}
+});
+
+export default IndicatorLegend;
